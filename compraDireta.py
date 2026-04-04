af66b90a0385a00697a0a6da4d7da2a75da4bacf
Gustavo Silva <gustavosilva.enge@gmail.com>
2026-04-04 20:32:52 -0300

    🎨Melhoria da estrutura de RMS pendente e iniciando a criação de processo

--- compraDireta.py
+++ compraDireta.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
@@ -19,19 +20,25 @@ navegador.find_element(By.NAME, 'senha').send_keys(dados.senha)
 botaoConfirmar = navegador.find_element(By.NAME, 'enviar').click()
 
 menu = navegador.current_window_handle
-anterior = navegador.window_handles
+abas_antes = navegador.window_handles
 
 espera.until(
     EC.element_to_be_clickable((By.LINK_TEXT, "Adicionar itens nos processos de compra"))
 ).click()
 
-espera.until(EC.new_window_is_opened(anterior))
+espera.until(EC.new_window_is_opened(abas_antes))
 
-proxima= next(aba for aba in navegador.window_handles if aba not in anterior)
-navegador.switch_to.window(proxima)
+abas_depois = navegador.window_handles
+nova_aba = next(aba for aba in abas_depois if aba not in abas_antes)
 
-temRMS = navegador.find_element(By.CLASS_NAME, "zebra2")
-if temRMS:
+navegador.switch_to.window(nova_aba)
+
+try:
+    navegador.find_element(By.CLASS_NAME, "zebra2")
+    existe = True
+except NoSuchElementException:
+    existe = False 
+if existe:
     navegador.close()
     espera.until(EC.number_of_windows_to_be(1))
     navegador.switch_to.window(menu)
@@ -40,5 +47,19 @@ else:
     navegador.close()
     espera.until(EC.number_of_windows_to_be(1))
     navegador.switch_to.window(menu)
+
+espera.until(
+    EC.element_to_be_clickable((By.LINK_TEXT, "Entrada de Processos"))
+).click()
+
+espera.until(EC.new_window_is_opened(abas_antes))
+abas_depois = navegador.window_handles
+nova_aba = next(aba for aba in abas_depois if aba not in abas_antes)
+navegador.switch_to.window(nova_aba)
+
+espera.until(
+    EC.element_to_be_clickable((By.XPATH, "//a[@title='Adicionar']"))
+).click()
+
 
 sleep(10)
